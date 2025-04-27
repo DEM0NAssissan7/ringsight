5aea10553e0347f00ae8b30c804e8e5ce0252596
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-04-27 15:31:33 -0500

    move meal things from metabolism into meal

--- metabolism/glucose.js
+++
@@ -1,7 +0,0 @@
-// This is an untested, experimental feature, but it has a lot of potential
-// We tell the user, depending on the current blood sugar & stage in the digestion/insulin
-// We use the current drop in blood sugar to then compensate using glucose
-
-function get_glucose() {
-    
-}
